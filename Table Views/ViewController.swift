1d15c52dc738574a5df001f7e6563247db09c0a7
Srinivas, Thirumalaa <SrinivaT@advisory.com>
2014-12-18 15:55:40 -0600

    First commit on table views iOS project.

--- Table Views/ViewController.swift
+++ Table Views/ViewController.swift
@@ -8,16 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    var tableCellContent = ["cell one", "cell two", "cell three", "cell four"]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableCellContent.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        
+        cell.textLabel?.text = tableCellContent[indexPath.row]
+        
+        return cell
+    }
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
